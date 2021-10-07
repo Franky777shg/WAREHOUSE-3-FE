2528e7b7bd9ec235fec7c256ebb20b55620e8040
manridho <usman.manridho@gmail.com>
2021-10-07 10:51:37 +0700

    As a user, I want to be able to change my password

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/styles/index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import {Provider} from 'react-redux'
+import { createStore, applyMiddleware} from 'redux'
+import allReducer from './redux/reducers'
+import ReduxThunk from 'redux-thunk'
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const globalState = createStore(allReducer, applyMiddleware(ReduxThunk))
+
 ReactDOM.render(
-  <div>
-    <App />
-  </div>,
+  <Provider store={globalState}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
