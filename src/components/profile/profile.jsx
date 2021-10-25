54386dffee1a28585326a298b997f32c757c226d
HirdzanFrost <frostderek888@gmail.com>
2021-10-25 21:07:20 +0700

    fixing bug address, transaction list

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -29,6 +29,7 @@ export default class Profile extends React.Component {
       activeTab: "",
       isAddAddress: false,
       isLoading: false,
+      editAddressModal: false
     };
   }
 
@@ -51,6 +52,7 @@ export default class Profile extends React.Component {
       .then((res) => {
         console.log(res.data);
         this.setState({ dataaddress: res.data });
+        console.log(this.state.dataaddress)
       })
       .catch((err) => {
         console.log(err);
@@ -110,11 +112,47 @@ export default class Profile extends React.Component {
       });
   };
 
+  onEditAddress = () => {
+    let editaddressData = {
+      idaddress: this.state.idEdit,
+      address: this.refs.alamat.value,
+      kecamatan: this.refs.kecamatan.value,
+      kabupaten: this.refs.kabupaten.value,
+      provinsi: this.refs.provinsi.value,
+      hp: this.refs.hp.value,
+      penerima: this.refs.penerima.value,
+      tipe_alamat: this.refs.jenis_alamat.value,
+    };
+    
+    console.log(editaddressData);
+
+    let token = localStorage.getItem("token");
+    Axios.post(`${URL_API}/get-update-user-address/`, editaddressData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        this.setState({ dataaddress: res.data});
+        this.setState({ editAddressModal: false });
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
   handleCloseAdd = () => {
     this.setState({ isAddAddress: false });
   };
 
+  handleCloseEdit = () => {
+    this.setState({ editAddressModal: false });
+  };
+
   render() {
+    console.log(this.state.idEdit)
     return (
       <div>
         <Card className="border-0">
@@ -155,7 +193,8 @@ export default class Profile extends React.Component {
             <div style={style.addressWrapper} className="mt-2">
               {this.state.dataaddress.map((item) => {
                 return (
-                  <Alert
+                  <Alert 
+                    onClick={() => this.setState({ editAddressModal: true , idEdit : item.id_address})}
                     style={style.addressItem}
                     className="p-2 bg-white text-secondary"
                   >
@@ -236,6 +275,74 @@ export default class Profile extends React.Component {
             </Button>
             <Button variant="primary" onClick={this.onAddAddress}>
               Tambah alamat
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        {/* Edit address modal */}
+        <Modal show={this.state.editAddressModal} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Address</Modal.Title>
+          </Modal.Header>
+          {this.state.dataaddress.map((item) => {
+          if(this.state.idEdit === item.id_address){
+              return(    
+          <Modal.Body>
+            <Row className="mb-2">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label className="myLabel">Nama penerima</Form.Label>
+                <Form.Control size="sm" type="text" ref="penerima" defaultValue={item.package_recipient} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label className="myLabel">HP penerima</Form.Label>
+                <Form.Control size="sm" type="text" ref="hp" defaultValue={item.address_phone} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-2">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label className="myLabel">Jenis alamat</Form.Label>
+                <Form.Select ref="jenis_alamat" defaultValue={item.address_type}>
+                  <option value="Rumah">Rumah</option>
+                  <option value="Kantor">Kantor</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label className="myLabel">Provinsi</Form.Label>
+                <Form.Control size="sm" type="text" ref="provinsi" defaultValue={item.provinsi} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-2">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label className="myLabel">Kecamatan</Form.Label>
+                <Form.Control size="sm" type="text" ref="kecamatan" defaultValue={item.kecamatan} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label className="myLabel">Kabupaten</Form.Label>
+                <Form.Control size="sm" type="text" ref="kabupaten" defaultValue={item.kabupaten}/>
+              </Form.Group>
+            </Row>
+            <Row className="mb-2">
+              <Form.Group>
+                <Form.Label className="myLabel">Alamat lengkap</Form.Label>
+                <Form.Control size="sm" type="text" ref="alamat" defaultValue={item.address} />
+              </Form.Group>
+            </Row>
+          </Modal.Body>
+            )
+          }
+            
+          
+             
+          })}
+          <Modal.Footer className="py-1">
+            <Button variant="secondary" onClick={this.handleCloseEdit}>
+              Batal
+            </Button>
+            <Button variant="primary" onClick={this.onEditAddress}>
+            Update            
             </Button>
           </Modal.Footer>
         </Modal>
